1dc2dda77ee907cdb2a81b156e78f081f4dfe95f
tfkfan <tfkfan@tfkfan>
2023-11-15 22:59:43 +0300

    refactored #11

--- src/main/java/com/tfkfan/nettywebgame/service/GameRoomManagementService.java
+++ src/main/java/com/tfkfan/nettywebgame/service/GameRoomManagementService.java
@@ -39,35 +39,31 @@ public class GameRoomManagementService {
     }
 
     public void addPlayerToWait(GameRoomJoinEvent event) {
-        try {
-            sessionQueue.add(event);
-            event.getSession().getChannel().writeAndFlush(new OutcomingMessage(MessageType.JOIN_WAIT));
+        sessionQueue.add(event);
+        event.getSession().getChannel().writeAndFlush(new OutcomingMessage(MessageType.JOIN_WAIT));
 
-            if (sessionQueue.size() < applicationProperties.getRoom().getMaxPlayers())
-                return;
+        if (sessionQueue.size() < applicationProperties.getRoom().getMaxPlayers())
+            return;
 
-            final GameMap gameMap = new GameMap();
-            final DefaultGameRoom room = new DefaultGameRoom(gameMap,
-                    UUID.randomUUID(), GameRoomManagementService.this, schedulerService, applicationProperties.getRoom());
-            gameRoomMap.put(room.key(), room);
+        final GameMap gameMap = new GameMap();
+        final DefaultGameRoom room = new DefaultGameRoom(gameMap,
+                UUID.randomUUID(), GameRoomManagementService.this, schedulerService, applicationProperties.getRoom());
+        gameRoomMap.put(room.key(), room);
 
-            final List<PlayerSession> playerSessions = new ArrayList<>();
-            while (playerSessions.size() != applicationProperties.getRoom().getMaxPlayers()) {
-                final GameRoomJoinEvent evt = sessionQueue.remove();
-                final PlayerSession ps = evt.getSession();
-                ps.setRoomKey(room.key());
-                ps.setPlayer(playerFactory.create(gameMap.nextPlayerId(), evt, room, ps));
-                playerSessions.add(ps);
-            }
-
-            gameChannelMode.apply(playerSessions);
-            room.onRoomCreated(playerSessions);
-            room.start(applicationProperties.getRoom().getStartDelay(),
-                    applicationProperties.getRoom().getEndDelay(),
-                    applicationProperties.getRoom().getLoopRate());
-        } catch (Exception e) {
-            log.warn("Queue interrupted", e);
+        final List<PlayerSession> playerSessions = new ArrayList<>();
+        while (playerSessions.size() != applicationProperties.getRoom().getMaxPlayers()) {
+            final GameRoomJoinEvent evt = sessionQueue.remove();
+            final PlayerSession ps = evt.getSession();
+            ps.setRoomKey(room.key());
+            ps.setPlayer(playerFactory.create(gameMap.nextPlayerId(), evt, room, ps));
+            playerSessions.add(ps);
         }
+
+        gameChannelMode.apply(playerSessions);
+        room.onRoomCreated(playerSessions);
+        room.start(applicationProperties.getRoom().getStartDelay(),
+                applicationProperties.getRoom().getEndDelay(),
+                applicationProperties.getRoom().getLoopRate());
     }
 
     public void onBattleEnd(GameRoom room) {
